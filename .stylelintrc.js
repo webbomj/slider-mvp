65cb646ecf4567bbb595198ec59e35a4adddd559
Andrey Lapin <nethack@mail.ru>
2022-08-10 10:38:31 +0400

    Used styled lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-rational-order',
     'stylelint-prettier/recommended',
+    '',
   ],
   plugins: ['stylelint-order', 'stylelint-scss'],
   rules: {
